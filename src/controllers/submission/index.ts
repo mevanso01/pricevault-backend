29b8deed9acd9e2ca84085a995e5f82617d40536
zhilingspicer <zhilingspicer@outlook.com>
2022-01-14 10:16:50 +0800

    Code refactored

--- src/controllers/submission/index.ts
+++ src/controllers/submission/index.ts
@@ -5,79 +5,78 @@ import * as Mongoose from 'mongoose';
 import Submission from "../../models/Submission";
 
 export default class SubmissionController {
-    router: any = null;
-    authMiddleware: any = null;
+  router: any = null;
+  authMiddleware: any = null;
 
-    constructor(authMiddleware: any = null) {
-        this.router = express.Router();
-        this.authMiddleware = authMiddleware;
+  constructor(authMiddleware: any = null) {
+    this.router = express.Router();
+    this.authMiddleware = authMiddleware;
 
-        this.router.use(this.authMiddleware);
+    this.router.use(this.authMiddleware);
 
-        this.configure();
-    }
+    this.configure();
+  }
 
-    configure() {
-        /**
-         * POST: submit submissions
-         */
-        this.router.post('/',
-            body('items').notEmpty(),
-            async (req: express.Request, res: express.Response) => {
-                // Validate request payload
-                const errors = validationResult(req);
-                if (!errors.isEmpty()) {
-                    return res.json({
-                        success: false,
-                        errors: ['Invalid request payload']
-                    })
-                }
+  configure() {
+    /**
+     * POST: submit submissions
+     */
+    this.router.post('/',
+      body('items').notEmpty(),
+      async (req: express.Request, res: express.Response) => {
+        // Validate request payload
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.json({
+            success: false,
+            errors: ['Invalid request payload']
+          })
+        }
 
-                try {
-                    var items = JSON.parse(req.body.items);
-                    // Add userId field into each item
-                    var payload = items.map(obj=> ({ ...obj, userId: req.user.id }));
-                    
-                    await Submission.insertMany(payload, {ordered: false});
-                    
-                    res.json({
-                        success: true,
-                        errors: []
-                    });
-                    
-                } catch (err) {
-                    console.log(err);
-                    const errors = ['Failed to store into database'];
-                    if (err instanceof Mongoose.Error) {
-                        errors.push(err.message);
-                    } else {
-                        errors.push(err);
-                    }
-                    res.json({
-                        success: false,
-                        errors
-                    });
-                }
-            }
-        );
+        try {
+          const items = JSON.parse(req.body.items);
+          // Add userId field into each item
+          const payload = items.map(obj=> ({ ...obj, userId: req['user'].id }));
 
-        /**
-         * GET: get all submissions
-         */
-        this.router.get('/', this.authMiddleware, async (req: express.Request, res: express.Response) => {
-            const submission = await Submission.find().exec();
+          await Submission.insertMany(payload, {ordered: false});
 
-            if (!submission) {
-                return res.json({
-                    success:false,
-                    items: []
-                });
-            }
+          res.json({
+            success: true,
+            errors: []
+          });
+        } catch (err) {
+          console.log(err);
+          const errors = ['Failed to store into database'];
+          if (err instanceof Mongoose.Error) {
+            errors.push(err.message);
+          } else {
+            errors.push(err);
+          }
+          res.json({
+            success: false,
+            errors
+          });
+        }
+      }
+    );
 
-            res.json({
-                success: true,
-                items: submission
-            })
+    /**
+     * GET: get all submissions
+     */
+    this.router.get('/', this.authMiddleware, async (req: express.Request, res: express.Response) => {
+      const submission = await Submission.find().exec();
+
+      if (!submission) {
+        return res.json({
+          success:false,
+          items: []
         });
-    }
+      }
+
+      res.json({
+        success: true,
+        items: submission
+      })
+    });
+  }
 }
--- src/models/Submission.ts
+++ src/models/Submission.ts
@@ -1,20 +1,20 @@
 import * as Mongoose from "mongoose";
 
 const submission = new Mongoose.Schema({
-    tradeId: {
-        type: Number,
-        required: true
-    },
-    valuation: {
-        type: Number,
-        required: true
-    },
-    forward: Number,
-    userId: {
-        type: Mongoose.Schema.Types.ObjectId,
-        ref: 'users',
-        required: true
-    },
+  tradeId: {
+    type: Number,
+    required: true
+  },
+  valuation: {
+    type: Number,
+    required: true
+  },
+  forward: Number,
+  userId: {
+    type: Mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
 },{ timestamps: { createdAt: 'createdAt' } })
 
 export default Mongoose.model("submission", submission);
